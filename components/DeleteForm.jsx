67a2d52637377c6bc916268d13d5e33d6258350b
Guillaume <guillaumegozillon@gmail.com>
2022-02-21 17:04:45 +0100

    Add: CSS for deleteForm

--- components/DeleteForm.jsx
+++ components/DeleteForm.jsx
@@ -18,16 +18,20 @@ export default function DeleteForm() {
   }
 
   return (
-    <div>
+    <div className='flex flex-col justify-start items-start border w-2/3 mx-auto mt-8 p-4 rounded-lg bg-gray-50'>
+      <h2 className='text-xl font-semibold mb-4'>
+        Supprimez vos annonces en cliquant sur la croix : X
+      </h2>
       {postLists.map((post, i) => (
         <div key={i}>
-          <div>
-            <div>
+          <div className='flex items-center'>
+            <div className='mr-4'>
               <h1>{post.title}</h1>
             </div>
             <div>
               {post.author?.id === auth.currentUser.uid && (
                 <button
+                  className='bg-slate-200 p-1 rounded-full h-8 w-8 cursor-pointer hover:bg-slate-400'
                   onClick={() => {
                     deletePost(post.id)
                   }}
